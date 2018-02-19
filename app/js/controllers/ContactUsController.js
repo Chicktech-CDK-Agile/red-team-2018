ac28c91689a266220116b135d80e534a9359c155
chicktech <kavita.nautiyal@cdk.com>
2018-02-19 15:08:58 -0800

    improve contact page

--- app/js/controllers/ContactUsController.js
+++ app/js/controllers/ContactUsController.js
@@ -2,6 +2,38 @@
  * Created by hensleym on 1/13/16.
  */
 angular.module('myApp.controllers')
-    .controller('ContactUsCtrl', ['$scope', 'navService', function($scope, navService) {
-        navService.setActiveTab("contactus");
+    .controller('ContactUsCtrl', ['$scope', '$modal', 'navService', function($scope, $modal, navService) {
+        $scope.contactinfo = {
+            FirstName: '',
+            LastName: '',
+            PhoneNumber:'',
+            Email:'',
+            Comments:'',
+            Birthdate:'',
+            Gender:''
+        };
+        $scope.contactMe = function(widget){
+            console.log(widget);
+
+
+            var copy = angular.copy(widget);
+            var modalInstance = $modal.open({
+                templateUrl: 'app/partials/modalcontent.html',
+                controller: 'ModalInstanceCtrl',
+                resolve: {
+                    widget: function() {
+                        return copy;
+                    }
+                }
+            });
+            $scope.contactinfo.FirstName= '';
+            $scope.contactinfo.LastName= '';
+            $scope.contactinfo.PhoneNumber= '';
+            $scope.contactinfo.Email= '';
+            $scope.contactinfo.Comments= '';
+            $scope.contactinfo.Birthdate= '';
+            $scope.contactinfo.Gender= '';
+        };
+
     }]);
+
